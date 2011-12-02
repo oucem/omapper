187f7a93add0c7158b9a04fab0cede90cbc32f41
sachin.magician <sachin.magician@gmail.com>
2011-12-02 15:20:07 +0000

    Removed duplicate methods and logic...and some new exceptions

---
+++  omapper/OMapper/src/org/omapper/exception/NonMappableTargetBeanException.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package org.omapper.exception;
+
+/**
+ * @author Sachin
+ *
+ */
+public class NonMappableTargetBeanException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4494097297587455660L;
+
+	/**
+	 * 
+	 */
+	public NonMappableTargetBeanException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public NonMappableTargetBeanException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public NonMappableTargetBeanException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public NonMappableTargetBeanException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
