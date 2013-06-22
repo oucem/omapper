9f0d707fc5ed9439ee4bdd56fc0de558c200e1fb
sachin.magician@gmail.com <sachin.magician@gmail.com@91d0790d-64d7-0352-0882-33aa978ef33b>
2013-06-22 06:03:55 +0000

    Added javadocs

--- trunk/ omapper/OMapper/src/org/omapper/enums/FieldType.java
+++ trunk/ omapper/OMapper/src/org/omapper/enums/FieldType.java
@@ -5,18 +5,31 @@ package org.omapper.enums;
 
 
 /**
+ * The Enum FieldType.
+ * 
  * @author Sachin
- *
  */
 public enum FieldType {
 	
+	/** The java. */
 	JAVA,
+	
+	/** The user. */
 	USER,
+	
+	/** The enum. */
 	ENUM,
+	
+	/** The array. */
 	ARRAY,
+	
+	/** The collection. */
 	COLLECTION,
+	
+	/** The map. */
 	MAP,
 	/*This field type indicates that the field type is an interface or abstract which can not be instantiated and needs an implementation */
+	/** The template. */
 	TEMPLATE
 
 }
--- trunk/ omapper/OMapper/src/org/omapper/enums/MappingType.java
+++ trunk/ omapper/OMapper/src/org/omapper/enums/MappingType.java
@@ -1,8 +1,14 @@
 package org.omapper.enums;
 
 
+/**
+ * The Enum MappingType.
+ */
 public enum MappingType {
 
+	/** The source. */
 	SOURCE,
+	
+	/** The target. */
 	TARGET
 }
